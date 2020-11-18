6eb9dde44c6668b6156d2b5ff19f4f12685ad766
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-18 23:18:10 +0530

    Added dailyEmpWage calculation part

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,9 +3,15 @@ echo "Welcome to Employee Wage Computation"
 attendance=$((RANDOM%2))
 present=1
 absent=0
+wagePerHour=20
+fullDayHour=8
+
 if (( $attendance == $present ))
 then
 	echo "Employee is present-$attendance"
 else
 	echo "Employee is absent-$attendance"
 fi
+
+dailyEmpWage=$(( $wagePerHour * $fullDayHour ))
+
