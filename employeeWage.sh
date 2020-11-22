928c0b33b829365d341cff9e54f28e551ed01e06
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-22 11:16:28 +0530

    Added code for calculating wage for month and added function for calculating empWage

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,36 +1,57 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation"
 
-attendance=$((RANDOM%3))
-
-present=1
+presentForPartTime=1
+presentForFullTime=2
 absent=0
 wagePerHour=20
 fullTimeHour=8
 partTimeHour=4
+empWorkingDays=0
+maxWorkingDays=20
 
-#partTimeEmp mean 1
 
 
-if (( $attendance == $present ))
-then
-	echo "Employee is present-$attendance"
-else
-	echo "Employee is absent-$attendance"
-fi
+function dailyEmpWage()
+{
 
-#dailyfullTimeEmpWage=$(( $fullTimeHour	* $wagePerHour ))
-#dailyPartTimeEmpWage=$(( $partTimeHour * $wagePerHour))
+	empWorkHours=$1
+	dailyEmpWage=$(( $empWorkHours * $wagePerHour ))
+	echo $dailyEmpWage
 
-case $attendance in
+}
 
-	1)empWorkHour=4 ;;
-	2)empWorkHour=8 ;;
-		*)empWorkHour=0
 
-esac
-dailyEmpWage=$(( $wagePerHour * $empWorkHour ))
-echo $dailyEmpWage
+
+
+while (( $empWorkingDays <= $maxWorkingDays ))
+do
+	attendance=$((RANDOM%3))
+	if (( $attendance == $presentForPartTime ))
+	then
+		echo "Employee is present-$attendance, for part time"
+
+	elif (( $attendance == $presentForFullTime ))
+	then
+		echo "Employee is absent-$attendance, for full time"
+	else
+		echo "Employee is absent-$attendance"
+	fi
+
+	case $attendance in
+
+		1)empWorkHour=4 ;;
+		2)empWorkHour=8 ;;
+			*)empWorkHour=0
+
+	esac
+
+	dailyEmpWage $attendance
+
+((empWorkingDays++))
+done
+
+
 
 
 
